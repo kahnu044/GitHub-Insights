6271f4a239919aaa6516c6bc634901a9684b0895
kahnu044 <djkanha044@gmail.com>
2023-11-13 23:42:49 +0530

    Plugin Files Added

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0e0c2a597f91e400f2f7');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '8225449f0d1a2166d773');
