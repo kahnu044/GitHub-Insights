1fb04ffc89a6635e17afa5b107f93b9cde071d8d
kahnu044 <djkanha044@gmail.com>
2023-11-14 10:25:57 +0530

    Info List Added

--- src/edit.js
+++ src/edit.js
@@ -1,16 +1,44 @@
 import { __ } from '@wordpress/i18n';
-
-import { useBlockProps } from '@wordpress/block-editor';
+import { useState } from 'react';
+import { useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, FormTokenField } from '@wordpress/components';
 
 import './editor.scss';
 
+const repoInfoField = [
+	'name',
+	'watchers_count',
+	'language',
+	'forks',
+	'html_url',
+	'url',
+];
+
 export default function Edit() {
+
+	const [repoInfoLists, setRepoInfoLists] = useState([]);
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Github Insights – Hello! from github insights',
-				'github-insights'
-			) }
-		</p>
+		<div {...useBlockProps()}>
+			<InspectorControls>
+				<PanelBody title="GitHub Repo Settings">
+					<FormTokenField
+						label="Select Info Fields"
+						value={repoInfoLists}
+						suggestions={repoInfoField}
+						onChange={(tokens) => setRepoInfoLists(tokens)}
+						placeholder="Select Fields Name"
+					/>
+				</PanelBody>
+			</InspectorControls>
+
+			<RichText
+				tagName="p"
+				// value={attributes.heading}
+				// allowedFormats={['core/bold', 'core/italic']}
+				// onChange={(heading) => setAttributes({ heading })}
+				placeholder="Enter heading here..."
+			/>
+		</div>
 	);
 }
