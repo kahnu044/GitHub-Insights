6aa079a4eae46d7376707e72e817d847991d5601
kahnu044 <djkanha044@gmail.com>
2023-11-18 15:28:23 +0530

    Update the build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bea17a7a1986e87fc2cf');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9b341764fa257fbd669a');
